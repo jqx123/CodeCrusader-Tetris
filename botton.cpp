9454170cf84cf16d7e40c9052e2f6d4dffc3ba33
jiubenyamei <jiuben@123.com>
2023-07-29 00:48:00 +0800

    botton.cpp

--- botton.cpp
+++ botton.cpp
@@ -14,9 +14,78 @@ void botton_exit(int x, int y, int w, int h) {}
 // 游戏界面的按钮
 
 // 暂停，音效，重玩
-void botton_pause(int x, int y, int w, int h) {}
-void botton_voice(int x, int y, int w, int h) {}
-int botton_reset(int x, int y, int w, int h) {}
+void botton_pause(int x, int y, int w, int h) {
+	char s[] = "暂停";
+	ExMessage msg = { 0 };
+	int W = textwidth(s);
+	int H = textheight(s);
+	settextcolor(BLACK);
+	setbkmode(TRANSPARENT);
+	while (true) {
+		if (peekmessage(&msg, EX_MOUSE | EX_KEY)) {}
+		BeginBatchDraw();
+		if (msg.message == WM_LBUTTONDOWN && msg.x > x && msg.x<x + w && msg.y>y && msg.y < y + h) {
+			setfillcolor(RGB(70, 70, 70));
+			fillroundrect(x, y, x + w, y + h, 10, 10);
+		}
+		else {
+			setfillcolor(WHITE);
+			fillroundrect(x, y, x + w, y + h, 10, 10);
+
+		}
+		outtextxy((w - W) / 2 + x, (h - H) / 2 + y, s);
+		EndBatchDraw();
+	}
+	getchar();
+}
+void botton_voice(int x, int y, int w, int h) {
+	char s[] = "音量";
+	ExMessage msg = { 0 };
+	int W = textwidth(s);
+	int H = textheight(s);
+	settextcolor(BLACK);
+	setbkmode(TRANSPARENT);
+	while (true) {
+		if (peekmessage(&msg, EX_MOUSE | EX_KEY)) {}
+		BeginBatchDraw();
+		if (msg.message == WM_LBUTTONDOWN && msg.x > x && msg.x<x + w && msg.y>y && msg.y < y + h) {
+			setfillcolor(RGB(70, 70, 70));
+			fillroundrect(x, y, x + w, y + h, 10, 10);
+		}
+		else {
+			setfillcolor(WHITE);
+			fillroundrect(x, y, x + w, y + h, 10, 10);
+
+		}
+		outtextxy((w - W) / 2 + x, (h - H) / 2 + y, s);
+		EndBatchDraw();
+	}
+	getchar();
+}
+int botton_reset(int x, int y, int w, int h) {
+	char s[] = "重新开始";
+	ExMessage msg = { 0 };
+	int W = textwidth(s);
+	int H = textheight(s);
+	settextcolor(BLACK);
+	setbkmode(TRANSPARENT);
+	while (true) {
+		if (peekmessage(&msg, EX_MOUSE | EX_KEY)) {}
+		BeginBatchDraw();
+		if (msg.message == WM_LBUTTONDOWN && msg.x > x && msg.x<x + w && msg.y>y && msg.y < y + h) {
+			setfillcolor(RGB(70, 70, 70));
+			fillroundrect(x, y, x + w, y + h, 10, 10);
+		}
+		else {
+			setfillcolor(WHITE);
+			fillroundrect(x, y, x + w, y + h, 10, 10);
+
+		}
+		outtextxy((w - W) / 2 + x, (h - H) / 2 + y, s);
+		EndBatchDraw();
+	}
+	getchar();
+}
 
 // 左右下移，掉落 view
 int botton_moveleft(int x, int y, int w, int h) {}
