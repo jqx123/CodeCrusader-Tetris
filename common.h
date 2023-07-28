efeaabc9a712ea1a35d3f6c5cbc98a08ca230879
evil egg <2941863832@qq.com>
2023-07-28 11:52:17 +0800

    start_working

---
+++ common.h
@@ -0,0 +1,11 @@
+#ifndef COMMON_H
+#define COMMON_H
+//这里储存了公有变量
+struct config
+{
+    float voice;
+    float diff;
+    int choose;
+}config;
+
+#endif
---
+++ panel.cpp
@@ -0,0 +1,41 @@
+#include "panel.h"
+// 恢复设置（方块会探索下一个位置是否合法，不合法需恢复面板）
+bool panel::recoverPenal() {}
+// 是否着陆(是否碰到下边)
+bool panel::isAttachBottom() {}
+
+// 是否碰到左边
+bool panel::isAttachLeft() {}
+
+// 是否碰到右边
+bool panel::isAttachRight() {}
+
+// 随机获取方块形状
+char panel::randomShape() {}
+
+// 用方块数组给面板数组赋值
+bool panel::setPenal() {}
+
+// 方块动过后要把遗留面板信息擦除
+bool panel::erasePenal() {}
+
+// 随机创建方块的方法
+void panel::createCube() {}
+
+//!!!
+// 键盘响应
+void keyEvent() {}
+// 移动的方法,移动的过程中对m_penal的改变
+void panel::move(int dir) {}
+
+// 旋转的方法。。。如果能旋转，那么旋转，否则不转,调用rotate
+void panel::can_roll() {}
+
+// 方块停止
+void panel::stop() {}
+
+// 擦除满行
+void panel::erase() {}
+
+// 擦除完上面的图形整块坠落
+void panel::down(int level) {}
